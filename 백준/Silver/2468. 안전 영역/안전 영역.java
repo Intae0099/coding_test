e7517214c44341ce10db26755689c63dbfd8dc78
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-03-04 13:44:37 +0900

    [Silver I] Title: 안전 영역, Time: 212 ms, Memory: 34496 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,54 +1,60 @@
-import java.util.*;
 import java.awt.Point;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	static int N, result, max_height = 0;
+	static int N, max_num, result;
 	static int[][] map;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
+	static boolean[][] visit;
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
 	public static void main(String[] args) throws Exception{
+		//--------------솔루션 코드를 작성하세요.--------------------------------
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
+		max_num = 0;
 		int temp;
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				temp = Integer.parseInt(st.nextToken());
+				if(max_num < temp) max_num = temp;
 				map[i][j] = temp;
-				if(max_height < temp) {
-					max_height = temp;
-				}
 			}
 		}
 		
-		// 비온 높이가 0일때
-		result = 1;
+		result = 0;
 		
-		for (int i = 1; i < max_height; i++) {
-			check(i, new boolean[N][N]);
+		int temp_result;
+		for (int num = 0; num <= max_num; num++) {
+			visit = new boolean[N][N];
+			temp_result = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(map[i][j] > num && !visit[i][j]) {
+						visit[i][j] = true;
+						temp_result += 1;
+						bfs(i,j, num);
+					}
+				}
+			}
+			
+			if(result < temp_result) {
+				result = temp_result;
+			}
 		}
 		
 		System.out.println(result);
 		
 	}
-	private static void check(int height, boolean[][] visit) {
-		int temp_result = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(map[i][j] > height && !visit[i][j]) {
-					bfs(i, j, visit, height);
-					temp_result += 1;
-				}
-			}
-		}
-		if(result < temp_result) result = temp_result;
-	}
-	private static void bfs(int x, int y, boolean[][] visit, int height) {
-		Queue<Point> q = new ArrayDeque<>();
+	
+	
+	private static void bfs(int x, int y, int num) {
+		Queue<Point> q = new ArrayDeque<>(); 
 		q.add(new Point(x, y));
+		
 		Point p;
 		int nx, ny;
 		while(!q.isEmpty()) {
@@ -59,18 +65,29 @@ public class Main {
 			for (int i = 0; i < 4; i++) {
 				nx = x + dx[i];
 				ny = y + dy[i];
-				
 				if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 				
-				if(!visit[nx][ny] && map[nx][ny] > height) {
-					visit[nx][ny] = true;
-					q.add(new Point(nx, ny));
-				}
-						
+				if(visit[nx][ny]) continue;
+				if(map[nx][ny] <= num) continue;
+				
+				visit[nx][ny] = true;
+				q.add(new Point(nx, ny));
 			}
-			
 		}
-		
+
+	}
+
+
+	static void print() {
+		for (int i = 0; i < N; i++) {
+			System.out.println(Arrays.toString(map[i]));
+		}
+	}
+	
+	static void visit_print() {
+		for (int i = 0; i < N; i++) {
+			System.out.println(Arrays.toString(visit[i]));
+		}
 	}
 	
 }
