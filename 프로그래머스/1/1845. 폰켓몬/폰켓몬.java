b23a92a6439126ccb15e34835d40026dc0e44ef5
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-03-11 09:38:28 +0900

    [level 1] Title: 폰켓몬, Time: 18.37 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,13 +1,21 @@
-import java.util.HashSet;
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        HashSet<Integer> set = new HashSet<>();
-        for(int n : nums){
-            set.add(n);
+        HashMap<Integer, Integer> hash = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if(hash.containsKey(nums[i])) {
+				hash.replace(nums[i], hash.get(nums[i]) + 1); 
+			}
+            else{
+                hash.put(nums[i], 1);
+            }
+		}
+		if(hash.size() > nums.length / 2) {
+		    return nums.length / 2;	
+		}
+        else{
+            return hash.size();
         }
         
-        answer = (set.size() >= nums.length / 2) ? nums.length/2 : set.size();
-        return answer;
     }
 }
