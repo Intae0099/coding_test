12ddd4f80d8924a55f6898ff3af8a5b9f63bb194
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-03-26 11:34:03 +0900

    [D4] Title: 서로소 집합, Time: 336 ms, Memory: 105,928 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/README.md
@@ -0,0 +1,15 @@
+# [D4] 서로소 집합 - 3289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJKA6qr2oDFAWr) 
+
+### 성능 요약
+
+메모리: 105,928 KB, 시간: 336 ms, 코드길이: 1,357 Bytes
+
+### 제출 일자
+
+2025-03-26 11:33
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+	static int[] parents;
+	static int N, M;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb;
+		int T = Integer.parseInt(br.readLine());
+		int ord, a, b, f1, f2;
+		for (int t = 1; t < T+1; t++) {
+			sb = new StringBuilder();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			make();
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				ord = Integer.parseInt(st.nextToken());
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
+				if(ord == 0) {
+					union(a,b);
+				}
+				else {
+					f1 = find(a);
+					f2 = find(b);
+					if(f1 == f2) {
+						sb.append(1);
+					}
+					else {
+						sb.append(0);
+					}
+				}
+			}
+			
+			System.out.println("#" + t + " " + sb);
+		}
+	}
+	
+	private static int find(int a) {
+		if(a == parents[a]) return a;
+		return parents[a] = find(parents[a]);
+	}
+
+	private static void union(int a, int b) {
+		int aRoot, bRoot;
+		aRoot = find(a);
+		bRoot = find(b);
+		parents[bRoot] = aRoot;
+		
+	}
+
+	private static void make() {
+		parents = new int[N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			parents[i] = i;
+		}
+	}
+}
