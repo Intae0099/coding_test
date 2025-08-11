b5ff9c1dd4032a45dc414fd81ea10e6b8c633193
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-08-11 23:17:53 +0900

    [Silver I] Title: 볼 모으기, Time: 164 ms, Memory: 18488 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -11,23 +11,36 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         arr = br.readLine().toCharArray();
 
-        result = Math.min(result, check_move('R'));
-        result = Math.min(result, check_move('B'));
+        result = Math.min(result, check_move('R', true));
+        result = Math.min(result, check_move('R', false));
+        result = Math.min(result, check_move('B', true));
+        result = Math.min(result, check_move('B', false));
 
         System.out.println(result);
 
     }
 
-    public static int check_move(char color){
-        int idx = N-1;
-        while (idx >= 0 && arr[idx] == color){
-            idx--;
+    // dire == true : 오른족
+    public static int check_move(char color, boolean dire){
+        int idx = 0;
+        if(dire){
+            idx = N-1;
         }
 
+        if (dire) {
+            while (idx >= 0 && arr[idx] == color) idx--;
+        } else {
+            while (idx < N && arr[idx] == color) idx++;
+        }
+        
         int cnt = 0;
-        for (int i = 0; i < idx; i++) {
-            if(arr[i] == color){
-                cnt++;
+        if (dire) {
+            for (int i = 0; i <= idx; i++) {
+                if (arr[i] == color) cnt++;
+            }
+        } else {
+            for (int i = idx; i < N; i++) {
+                if (arr[i] == color) cnt++;
             }
         }
 
