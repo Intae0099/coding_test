2172c285acddddc28bd84cd79231653fd3f14b94
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-08-15 15:35:17 +0900

    [Silver I] Title: 단축키 지정, Time: 68 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.java
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -2,88 +2,60 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     static int N;
-    static HashSet<Character> set;
-    static StringBuilder sb;
+    static HashSet<Character> set = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        set = new HashSet<>();
-        sb = new StringBuilder();
-        String temp;
-        String total_word;
         for (int i = 0; i < N; i++) {
-            total_word = br.readLine();
-            if(first_check(total_word)){
-                first_do(total_word);
-            }
-            else{
-                second_do(total_word);
-            }
+            String line = br.readLine();
+            processLine(line);
             sb.append("\n");
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    public static boolean first_check(String words){
-        StringTokenizer st = new StringTokenizer(words);
-        while(st.hasMoreTokens()){
-            String temp = st.nextToken();
-            if(!set.contains(Character.toUpperCase(temp.charAt(0)))){
+    private static void processLine(String line) {
+        String[] words = line.split(" ");
+        boolean assigned = false;
 
-                return true;
+        // 1차: 각 단어의 첫 글자로 단축키 지정
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].isEmpty()) continue; // 빈 문자열 처리
+            char upper = Character.toUpperCase(words[i].charAt(0));
+            if (!set.contains(upper)) {
+                set.add(upper);
+                words[i] = "[" + words[i].charAt(0) + "]" + words[i].substring(1);
+                assigned = true;
+                break;
             }
         }
-        return false;
-    }
 
-    public static boolean second_check(String words){
-        for(char alpha : words.toCharArray()){
-            if(alpha == ' ') continue;
-            if(!set.contains(alpha)) return true;
-        }
-        return false;
-    }
-
-    public static void first_do(String words){
-        StringTokenizer st = new StringTokenizer(words);
-        boolean flag = false;
-        String temp;
-        while(st.hasMoreTokens()){
-            temp = st.nextToken();
-            if(!flag && !set.contains(Character.toUpperCase(temp.charAt(0)))){
-                set.add(Character.toUpperCase(temp.charAt(0)));
-                sb.append("[").append(temp.charAt(0)).append("]");
-                for (int j = 1; j < temp.length(); j++) {
-                    sb.append(temp.charAt(j));
+        // 2차: 첫 글자로 지정 불가하면, 왼쪽부터 가능한 알파벳 지정
+        if (!assigned) {
+            outer:
+            for (int i = 0; i < words.length; i++) {
+                StringBuilder tmp = new StringBuilder();
+                for (int j = 0; j < words[i].length(); j++) {
+                    char ch = words[i].charAt(j);
+                    char upper = Character.toUpperCase(ch);
+                    if (ch != ' ' && !set.contains(upper)) {
+                        set.add(upper);
+                        tmp.append("[").append(ch).append("]");
+                        tmp.append(words[i].substring(j + 1));
+                        words[i] = tmp.toString();
+                        assigned = true;
+                        break outer;
+                    }
+                    tmp.append(ch);
                 }
-                sb.append(" ");
-                flag = true;
-                continue;
             }
-            sb.append(temp).append(" ");
         }
 
+        sb.append(String.join(" ", words));
     }
-
-    public static void second_do(String words){
-        boolean flag = false;
-        for(char alpha : words.toCharArray()){
-            if(alpha == ' ') {
-                sb.append(alpha);
-                continue;
-            }
-            if(!flag && !set.contains(Character.toUpperCase(alpha))) {
-                sb.append("[").append(alpha).append("]");
-                set.add(Character.toUpperCase(alpha));
-                flag = true;
-                continue;
-            }
-            sb.append(alpha);
-        }
-    }
-
 }
