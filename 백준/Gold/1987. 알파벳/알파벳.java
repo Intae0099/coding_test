92cd869e43a32d339b141be96754ceb18b014d7b
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-09-15 20:23:56 +0900

    [Gold IV] Title: 알파벳, Time: 868 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,58 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     static int R, C, result;
     static char[][] board;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        board = new char[R][C];
 
-        String temp;
+        board = new char[R][C];
         for (int i = 0; i < R; i++) {
-            temp = br.readLine();
-            for (int j = 0; j < C; j++) {
-                board[i][j] = temp.charAt(j);
-            }
+            String temp = br.readLine();
+            for (int j = 0; j < C; j++) board[i][j] = temp.charAt(j);
         }
 
         boolean[] alpha_visited = new boolean[26];
-        boolean[][] board_visited = new boolean[R][C];
-
-        board_visited[0][0] = true;
         alpha_visited[board[0][0] - 'A'] = true;
 
-        result = 0;
-        dfs(0, 0, alpha_visited, board_visited, 1);
-
+        dfs(0, 0, alpha_visited, 1);
         System.out.println(result);
     }
 
-    public static void dfs(int x, int y, boolean[] alpha_visited, boolean[][] board_visited, int cnt){
-
-        int nx, ny;
-        for (int i = 0; i < 4; i++) {
-            nx = x + dx[i];
-            ny = y + dy[i];
-
-            if(nx < 0 || nx >= R || ny < 0 || ny >= C
-                    || board_visited[nx][ny] || alpha_visited[board[nx][ny] - 'A']) continue;
-
-            board_visited[nx][ny] = true;
-            alpha_visited[board[nx][ny] - 'A'] = true;
-            dfs(nx, ny, alpha_visited, board_visited, cnt + 1);
-            board_visited[nx][ny] = false;
-            alpha_visited[board[nx][ny] - 'A'] = false;
-
-        }
-
+    public static void dfs(int x, int y, boolean[] alpha_visited, int cnt) {
+        if (result == 26) return;
         result = Math.max(result, cnt);
 
-    }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+            int idx = board[nx][ny] - 'A';
+            if (alpha_visited[idx]) continue;
+
+            alpha_visited[idx] = true;
+            dfs(nx, ny, alpha_visited, cnt + 1);
+            alpha_visited[idx] = false;
+        }
+    }
 }
