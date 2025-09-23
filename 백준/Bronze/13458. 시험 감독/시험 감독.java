31463ec9ca2eeac384a48a9440f67650e73a13b0
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-09-23 20:34:11 +0900

    [Bronze II] Title: 시험 감독, Time: 360 ms, Memory: 121420 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, B, C;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) ;
+        }
+
+        st = new StringTokenizer(br.readLine());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        long result = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] -= B;
+            result++;
+            if(arr[i] > 0){
+                if(arr[i] % C == 0) result += (arr[i] / C);
+                else result += (arr[i] / C + 1);
+            }
+
+        }
+
+        System.out.println(result);
+
+
+    }
+}
