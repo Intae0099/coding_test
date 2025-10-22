c359ff0614984dfd8fe751a611414a4931b208e0
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-10-22 09:31:01 +0900

    [Silver I] Title: 주지수, Time: 600 ms, Memory: 124808 KB -BaekjoonHub

--- Java/백준/Silver/15724. 주지수/주지수.java
+++ Java/백준/Silver/15724. 주지수/주지수.java
@@ -5,9 +5,11 @@ public class Main {
 
     static int N, M;
     static int[][] map;
+    static StringBuilder sb;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N+1][M+1];
@@ -35,8 +37,10 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            System.out.println(prefix_sum[x2][y2] - prefix_sum[x1 - 1][y2] - prefix_sum[x2][y1 - 1] + prefix_sum[x1 - 1][y1 - 1]);
+            sb.append(prefix_sum[x2][y2] - prefix_sum[x1 - 1][y2] - prefix_sum[x2][y1 - 1] + prefix_sum[x1 - 1][y1 - 1]).append("\n");
         }
+
+        System.out.println(sb);
 
     }
 }
