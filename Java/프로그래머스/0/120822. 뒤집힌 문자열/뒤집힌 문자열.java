d4d1bf883ec879fc1e8642d12728370391c14a80
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-10-24 14:39:47 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.07 ms, Memory: 91.1 MB -BaekjoonHub

--- Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -2,10 +2,8 @@ import java.util.*;
 class Solution {
     public String solution(String my_string) {
         StringBuilder sb = new StringBuilder();
-        char[] alpha = my_string.toCharArray();
-        for(int i = alpha.length-1; i >= 0; i--){
-            sb.append(alpha[i]);
-        }
+        sb.append(my_string);
+        sb.reverse();
             
         return sb.toString();
     }
