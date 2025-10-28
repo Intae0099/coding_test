91aa6fea0d478595cefd6d2e36809cb1fa2e85f8
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-10-28 19:59:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.63 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.8 MB, 시간: 78.77 ms
+메모리: 96.9 MB, 시간: 86.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 15:16:04
+2025년 10월 28일 19:59:58
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,28 +1,22 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-		String answer = "";
-		HashMap<String, Integer> hash = new HashMap<>();
-		
-		for (String str : completion) {
-			if(! hash.containsKey(str)) {
-				hash.put(str, 1);
-			}
-			else {
-				hash.replace(str, hash.get(str) + 1);
-			}
-		}
-		for (String str : participant) {
-			if(! hash.containsKey(str) || hash.get(str) == 0) {
-				answer = str;
-                break;
-			}
-			else {
-				hash.replace(str, hash.get(str) - 1);
-			}
-		}
+        HashMap<String, Integer> user = new HashMap<>();
         
+        for(String word : participant){
+            user.put(word, user.getOrDefault(word, 0) + 1);
+        }
+        
+        for(String complet : completion){
+            user.put(complet, user.get(complet) - 1);
+        }
+        
+        String answer = "";
+        for(String temp : user.keySet()){
+            if(user.get(temp) > 0) {
+                answer = temp;
+                }
+        }
         return answer;
     }
 }
