491eb482a6c4952ae171f5c604b8b160d123aae0
TaeIn <81225723+Intae0099@users.noreply.github.com>
2025-11-24 20:13:52 +0900

    [Gold III] Title: 파티, Time: 476 ms, Memory: 53816 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,22 +1,22 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+public class Main {
 
     static class Node implements Comparable<Node>{
-        int next, weight;
-        Node(int next, int weight){
+        int next, dist;
+        Node(int next, int dist){
             this.next = next;
-            this.weight = weight;
+            this.dist = dist;
         }
 
         @Override
-        public int compareTo(Node node){
-            return this.weight - node.weight;
+        public int compareTo(Node o){
+            return Integer.compare(this.dist, o.dist);
         }
     }
 
-    static int N, M, X, result;
+    static int N, M, X;
     static ArrayList<Node>[] adj;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,49 +24,55 @@ public class Main{
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-        adj = new ArrayList[N+1];
-        result = 0;
-        for(int i = 0; i < N+1; i++) adj[i] = new ArrayList<>();
 
-        int a, b, c;
-        for(int i = 0; i < M; i++){
+        adj = new ArrayList[N+1];
+        for (int i = 0; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        int a, b, t;
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-            adj[a].add(new Node(b, c));
+            t = Integer.parseInt(st.nextToken());
+            adj[a].add(new Node(b, t));
         }
 
         int[] fromX = dijkstra(X);
         int result = 0;
-        for(int i = 1; i <= N; i++){
+        for (int i = 1; i <= N; i++) {
             if(i == X) continue;
             int[] toX = dijkstra(i);
-            int roundTrip = toX[X] + fromX[i];
-            result = Math.max(result, roundTrip);
+            int temp = fromX[i] + toX[X];
+            result = Math.max(temp, result);
         }
 
         System.out.println(result);
+
+
     }
-    static int[] dijkstra(int start){
+
+    public static int[] dijkstra(int start){
         int[] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
 
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(start, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
 
-        while(!q.isEmpty()){
-            Node now = q.poll();
-            if(now.weight > dist[now.next]) continue;
-
-            for(Node next : adj[now.next]){
-                if(dist[next.next] > dist[now.next] + next.weight){
-                    dist[next.next] = dist[now.next] + next.weight;
-                    q.add(new Node(next.next, dist[next.next]));
+        Node now;
+        while (!pq.isEmpty()){
+            now = pq.poll();
+            if(now.dist > dist[now.next]) continue;
+            for (Node next : adj[now.next]){
+                if(dist[next.next] > dist[now.next] + next.dist){
+                    dist[next.next] = dist[now.next] + next.dist;
+                    pq.add(new Node(next.next, dist[next.next]));
                 }
             }
         }
         return dist;
     }
+
 }
