a3bf226dc6e6119b3b9a33c0573755256b9de127
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-02-06 15:25:29 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 204 ms, Memory: 33056 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,49 +1,79 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, K;
-    static int[] prev = new int[100001];
-    static int[] time = new int[100001];
 
-    public static void main(String[] args) throws IOException {
+    static int N, K;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        bfs();
+        if(N == K){
+            System.out.println(0);
+            System.out.println(N);
+        }
+        else{
+            bfs();
+        }
+
+
     }
 
-    static void bfs() {
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[100001];
+    public static void bfs(){
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(N);
-        visited[N] = true;
-        prev[N] = -1;
 
-        while (!q.isEmpty()) {
+        int[] time = new int[100001];
+        int[] prev = new int[100001];
+        
+        Arrays.fill(time, 100001);
+        time[N] = 0;
+        
+        while (!q.isEmpty()){
             int now = q.poll();
 
-            if (now == K) {
-                System.out.println(time[now]);
+            if(now == K) break;
 
-                ArrayList<Integer> path = new ArrayList<>();
-                for (int i = now; i != -1; i = prev[i]) {
-                    path.add(i);
+            for (int next : new int[] {now + 1, now - 1, now * 2}) {
+                if(next < 0 || next > 100000) continue;
+                if(time[next] == 100001){
+                    time[next] = time[now] + 1;
+                    prev[next] = now;
+                    q.add(next);
+
                 }
-                Collections.reverse(path);
-                for (int p : path) System.out.print(p + " ");
-                return;
-            }
 
-            for (int next : new int[]{now - 1, now + 1, now * 2}) {
-                if (next < 0 || next > 100000 || visited[next]) continue;
-                visited[next] = true;
-                q.add(next);
-                prev[next] = now;
-                time[next] = time[now] + 1;
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(time[K]).append("\n");
+
+        int now = K;
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(K);
+        while (true){
+            now = prev[now];
+            if(now == N) break;
+            arr.add(now);
+
+        }
+
+        arr.add(N);
+
+//        for (int i : arr.reversed()){
+//            sb.append(i).append(" ");
+//        }
+
+        for (int i = arr.size() - 1; i >= 0; i--){
+            sb.append(arr.get(i)).append(" ");
+        }
+        System.out.println(sb);
+
+
     }
+
+
 }
