ea73213e981e9ebc61cc268b32b355ea2dc83335
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-04-28 14:53:45 +0900

    [Gold IV] Title: 동전 1, Time: 80 ms, Memory: 15488 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, K;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N+1][K+1];
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if(j - arr[i-1] >= 0) dp[i][j] += dp[i][j - arr[i - 1]];
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+
+}
