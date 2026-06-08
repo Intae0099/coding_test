c512ce6a5b052ea295e1b8cbdbfabcab6ca19bd1
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-06-08 13:21:07 +0900

    [level 3] Title: 부대복귀, Time: 79.43 ms, Memory: 209 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,55 +1,58 @@
 import java.util.*;
 class Solution {
-    static int result_len;
-    static int[] result;
+    static class Node{
+        int next, dist;
+        Node(int next, int dist){
+            this.next = next;
+            this.dist = dist;
+        }
+    }
+
     static ArrayList<Integer>[] adj;
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        adj = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
+        adj = new ArrayList[n+1];
+        for (int i = 0; i <= n ; i++) {
             adj[i] = new ArrayList<>();
         }
-        for (int[] road : roads){
+
+        for (int[] road : roads) {
             int a = road[0];
             int b = road[1];
-
             adj[a].add(b);
             adj[b].add(a);
         }
-        result_len = sources.length;
-        result = new int[result_len];
-
-        for (int i = 0; i < result_len; i++) {
-            if(sources[i] == destination) continue;
-            int dist = dijkstra(sources[i], n, roads, sources, destination);
-            result[i] = dist;
-        }
         
-        return result;
-    }
-    
-    public static int dijkstra(int start, int n, int[][] roads, int[] sources, int destination){
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[] {start, 0});
+        int[] result = new int[sources.length];
+        Arrays.fill(result, -1);
         boolean[] visited = new boolean[n+1];
-        visited[start] = true;
+        int[] cost = new int[n+1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(destination, 0));
 
         while (!q.isEmpty()){
-            int[] node = q.poll();
-            int now = node[0];
-            int dist = node[1];
-
-            if(now == destination){
-                return dist;
-            }
+            Node node = q.poll();
+            int now = node.next;
+            int dist = node.dist;
 
             for (int i : adj[now]) {
-                if(visited[i]) continue;
+                if (visited[i]) continue;
                 visited[i] = true;
-                q.add(new int[] {i, dist + 1});
+                cost[i] = dist + 1;
+                q.add(new Node(i, dist + 1));
             }
-
         }
 
-        return -1;
+        for (int i = 0; i < sources.length; i++) {
+            if(sources[i] == destination) {
+                result[i] = 0;
+                continue;
+            }
+            int now_cost = cost[sources[i]];
+            if(now_cost == Integer.MAX_VALUE) result[i] = -1;
+            else result[i] = now_cost;
+
+        }        
+        return result;
     }
 }
