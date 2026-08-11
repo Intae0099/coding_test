733655dcb51181cfc2ad26a90a2b8c744e3961df
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-08-11 20:09:57 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 25.94 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 92.8 MB, 시간: 19.08 ms
+메모리: 90.7 MB, 시간: 25.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 23일 14:18:59
+2026년 08월 11일 20:09:52
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,35 +1,53 @@
 import java.util.*;
+
 class Solution {
+    
+    class Node{
+        int w, end_time;
+        Node(int w, int end_time){
+            this.w = w;
+            this.end_time = end_time;
+        }
+        
+        @Override
+        public String toString(){
+            return "w : " + w + " , end_time : " + end_time;
+        }
+    }
+    
+    static int N;
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> q = new ArrayDeque<>();
-        int sum = 0;
+        N = truck_weights.length;
+        int now_w = 0;
+        ArrayDeque<Node> q = new ArrayDeque<>();
+        int cnt = 0;
+        int idx = 0;
         int time = 0;
-        int temp;
-        for (int i = 0; i < truck_weights.length; i++) {
-            temp = truck_weights[i];
-            while (true){
-                if(q.isEmpty()){
-                    q.add(temp);
-                    sum = temp;
-                    time += 1;
-                    break;
-                } else if (q.size() == bridge_length) {
-                    sum -= q.poll();
-                } else {
-                    if(sum + temp <= weight){
-                        q.add(temp);
-                        sum += temp;
-                        time += 1;
-                        break;
-                    }
-                    else{
-                        q.add(0);
-                        time+=1;
-                    }
+        while(cnt < N){
+            time++;
+            int len = q.size();
+            for(int i = 0; i < len; i++){
+                Node now = q.poll();
+                if(now.end_time > time){
+                    q.add(now);
+                }
+                else {
+                    now_w -= now.w;
+                    cnt++;
+                }
+                
+                
+            }
+            
+            if(idx < N){
+                if(q.isEmpty() || now_w + truck_weights[idx] <= weight){
+                    q.add(new Node(truck_weights[idx], time + bridge_length));
+                    now_w += truck_weights[idx];
+                    idx++;
                 }
             }
         }
-
-        return time + bridge_length;
+    
+        return time;
     }
 }
