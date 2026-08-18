5b6baa622db9f21490f1a8ac51c878ba613a8bc7
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-08-18 12:00:02 +0900

    [level 3] Title: 이중우선순위큐, Time: 72.54 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 142 MB, 시간: 76.14 ms
+메모리: 135 MB, 시간: 72.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 27일 09:09:09
+2026년 08월 18일 11:59:58
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,41 +1,34 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> min_q = new PriorityQueue<>();
-        PriorityQueue<Integer> max_q = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i = 0; i < operations.length; i++){
-            String[] ord = operations[i].split(" ");
-            int k = Integer.parseInt(ord[1]);
+        PriorityQueue<Integer> max_pq = new PriorityQueue<>((a1, a2) -> Integer.compare(a2, a1));
+        PriorityQueue<Integer> min_pq = new PriorityQueue<>();
+        for(String operation : operations){
+            String[] ord = operation.split(" ");
+            int num = Integer.parseInt(ord[1]);
             if(ord[0].equals("I")){
-                min_q.add(k);
-                max_q.add(k);
+                max_pq.add(num);
+                min_pq.add(num);
             }
             else{
-                if(k == -1){
-                    if(min_q.size() == 0) continue;
-                    int temp = min_q.poll();
-                    max_q.remove(temp);
+                if(max_pq.isEmpty()) continue;
+                if(num == 1){
+                    int temp = max_pq.poll();
+                    min_pq.remove(temp);
                 }
                 else{
-                    if(max_q.size() == 0) continue;
-                    int temp = max_q.poll();
-                    min_q.remove(temp);
+                    int temp = min_pq.poll();
+                    max_pq.remove(temp);
+                    
                 }
             }
         }
-        int[] result = new int[2];
-        result[0] = 0;
-        result[1] = 0;
-        if(max_q.size() > 0){
-            result[0] = max_q.poll();
-        }
-
         
-        if(min_q.size() > 0){
-            result[1] = min_q.poll();
-        }
-
-        return result;
+        int[] answer = new int[2];
+        if(!max_pq.isEmpty()) answer[0] = max_pq.poll();
+        if(!min_pq.isEmpty()) answer[1] = min_pq.poll();
+        
+        
+        return answer;
     }
 }
