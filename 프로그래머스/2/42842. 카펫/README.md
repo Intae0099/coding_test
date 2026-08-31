57485743da82b23f8cafbf7541cfb852e512dbb7
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-08-31 11:30:49 +0900

    [level 2] Title: 카펫, Time: 8.63 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 73.7 MB, 시간: 11.56 ms
+메모리: 82.2 MB, 시간: 8.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 15:35:28
+2026년 08월 31일 11:30:45
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,11 @@
-import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 1; i <= yellow; i++){
-            if(yellow % i == 0) arr.add(i);
+        for(int i = yellow; i > 0; i--){
+            if(yellow % i != 0) continue;
+            int row = i;
+            int col = yellow / row;
+            if((row + 2) * 2 + col * 2 == brown) return new int[] {row + 2, col + 2};
         }
-        int[] answer = new int[2];
-        // i : 세로길이
-        for(int i : arr){
-            if(brown == 4 + i * 2 + (yellow / i) * 2){
-                answer[0] = yellow / i + 2;
-                answer[1] = i + 2;
-                break;
-            }
-        }
-        return answer;
+        return null;
     }
 }
