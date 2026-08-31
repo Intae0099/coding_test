617b359aaaf9e0d5971286e408fe0ddbf2f7602b
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-08-31 12:09:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.31 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,54 +1,45 @@
 import java.util.*;
-
 class Solution {
-    static int N;
     static ArrayList<Integer>[] adj;
+    static int N, result = Integer.MAX_VALUE;
     public int solution(int n, int[][] wires) {
         N = n;
-        adj = new ArrayList[N+1];
-        for(int i = 0; i <= N; i++){
-            adj[i] = new ArrayList<>();
-        }
-        
+        adj = new ArrayList[n+1];
+        for(int i = 0; i <= n; i++) adj[i] = new ArrayList<>();
         for(int[] wire : wires){
-            int a = wire[0];
-            int b = wire[1];
-            adj[a].add(b);
-            adj[b].add(a);
+            adj[wire[0]].add(wire[1]);
+            adj[wire[1]].add(wire[0]);
         }
         
-        int answer = Integer.MAX_VALUE;
+        for(int[] wire : wires) check(wire[0], wire[1]);
         
-        for(int[] wire : wires){
-            int cnt1 = check(wire[0], wire[1]);
-            int cnt2 = check(wire[1], wire[0]);
-            int sub = (int) Math.abs(cnt1 - cnt2);
-            answer = Math.min(answer, sub);
-        }
-        
-        return answer;
+        return result;
     }
     
-    public static int check(int start, int end){
-        int cnt = 1;
+    public void check(int a, int b){
         boolean[] visited = new boolean[N+1];
+        visited[a] = true;
+        visited[b] = true;
+        int cnt1 = count(a, visited);
+        int cnt2 = count(b, visited);
+        result = Math.min(result, Math.abs(cnt1 - cnt2));
+    }
+    
+    public int count(int start, boolean[] visited){
         Queue<Integer> q = new ArrayDeque<>();
         q.add(start);
-        visited[start] = true;
-        
+        int cnt = 0;
         while(!q.isEmpty()){
             int now = q.poll();
-            if(now == end) continue;
-            for(int next : adj[now]){
-                if(!visited[next]){
-                    visited[next] = true;
-                    q.add(next);
+            for(int i : adj[now]){
+                if(!visited[i]){
+                    visited[i] = true;
+                    q.add(i);
                     cnt++;
                 }
             }
         }
         
         return cnt;
-        
     }
 }
