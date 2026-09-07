12b3fb159dbd3e27a6c41ef64e3baf9ce93575d0
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-09-07 14:31:49 +0900

    [level 1] Title: 체육복, Time: 3.71 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 84.4 MB, 시간: 0.33 ms
+메모리: 87.7 MB, 시간: 3.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 17일 16:13:24
+2026년 09월 07일 14:31:46
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,31 +1,31 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, 1);
-        for(int i : lost){
-            dp[i] -= 1;
-        }
-        for(int i : reserve){
-            dp[i] += 1;
+        int[] clothes = new int[n+1];
+        Arrays.fill(clothes, 1);
+        for(int now : reserve) clothes[now] = 2;
+        for(int now : lost) {
+            if(clothes[now] > 1) clothes[now] = 1;
+            else clothes[now] = 0;
         }
         
-        for(int i = 1; i <= n; i++){
-            if(dp[i] == 0){
-                if(dp[i-1] == 2) {
-                    dp[i] += 1;
-                    dp[i-1] -= 1;
-                }
-                else if(i + 1 <= n && dp[i+1] == 2){
-                    dp[i] += 1;
-                    dp[i+1] -= 1;
+        Arrays.sort(lost);
+        
+        for(int now : lost){
+            if(clothes[now] == 1) continue;
+            for(int i = now - 1; i <= now + 1; i += 2){
+                if(i < 1 || i > n) continue;
+                if(clothes[i] == 2){
+                    clothes[i] = 1;
+                    clothes[now] = 1;
+                    break;
                 }
             }
         }
-        for(int i = 1; i <= n; i++){
-            if(dp[i] > 0) answer += 1;
-        }
+        
+        int answer = 0;
+        for(int i = 1; i <= n; i++) if(clothes[i] > 0) answer++;
+        
         return answer;
     }
 }
