8c14bfb3fc5f347eb6d7828b404da28cce33c788
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-09-08 14:48:22 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,20 +1,14 @@
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int idx;
-        int len = name.length();
-        int move = len - 1;
-        for(int i = 0; i < len; i++){
+        int N = name.length();
+        int move = N - 1;
+        for(int i = 0; i < N; i++){
             answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-            
-            idx = i + 1;
-            while(idx < len && name.charAt(idx) == 'A'){
-                idx += 1;
-            }
-            // 앞이 더 빠른경우
-            move = Math.min(move, i * 2 + len - idx);
-            // 뒤가 더 빠른경우
-            move = Math.min(move, i + (len - idx) * 2);   
+            int idx = i + 1;
+            while(idx < N && name.charAt(idx) == 'A') idx++;
+            move = Math.min(move, i * 2 + N - idx);
+            move = Math.min(move, i + (N - idx) * 2);
         }
         return answer + move;
     }
